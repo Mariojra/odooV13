daec90de469419179a626ff01cc5a072de8b2ce9
Josue Aguilar <josueaguilar1198@gmail.com>
2020-07-15 14:43:22 -0600

    add demo/demo.xml add views/openacademy.xml ~ mod ir.model.access.csv mod __manifest__.py

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -25,11 +25,12 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'templates.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo.xml',
+        '/demo/demo.xml',
     ],
 }
