e3de15a4b9f26eabe298bab04490b26b96ba40c2
Josue Aguilar <josueaguilar1198@gmail.com>
2020-07-24 22:59:23 -0600

    [ADD] session_board.xml ~ [MOD] __manifest__.py

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -30,6 +30,7 @@
         'templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
+        'views/session_board.xml',
         'views/reports.xml',
     ],
     # only loaded in demonstration mode
